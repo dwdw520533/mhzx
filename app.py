78492ce96968e82223f9058f3583432039e85a69
dongwei <dongwei@gstianfu.com>
2018-10-12 14:47:27 +0800

    bug fix

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from tool.api import api_wrap, APIResult
 from flask import Flask, request, Response, redirect
 from ops.user import register_user, change_password, back_password
 
-app = Flask("mhzx", static_url_path="/static")
+app = Flask("mhzx", static_folder="", static_url_path="")
 
 
 def expose(func):
@@ -54,14 +54,14 @@ def send_update_file(path):
 def index():
     return redirect("/static/index.html")
 
-#
-# @app.route('/static/<path:path>')
-# def send_static(path):
-#     return send_static_file(path)
+
+@app.route('/static/<path:path>')
+def send_static(path):
+    return send_static_file(path)
 
 
 @app.route('/update/<path:path>')
-def send_update_static(path):
+def send_update(path):
     return send_update_file(path)
 
 
