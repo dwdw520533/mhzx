b5c30812271283e6d8ff30e2c32cc0a1eada9483
dongwei <569293863@qq.com>
2018-10-13 22:54:17 +0800

    add code

--- ops/data.py
+++ ops/data.py
@@ -5,11 +5,11 @@ import pandas
 
 
 def query_role(user_id):
-    file_name = os.path.join(conf.LINUX_DATA_DIR, "role.txt")
-    if not os.path.exists(file_name):
-        return []
-    df = pandas.read_csv(file_name, skiprows=1, encoding="utf-8")
-    if not df.empty:
+    # file_name = os.path.join(conf.LINUX_DATA_DIR, "role.txt")
+    # if not os.path.exists(file_name):
+    #     return []
+    df = pandas.read_csv("ops/role.txt", skiprows=1, encoding="utf-8")
+    if df.empty:
         return []
     df_ret = df.loc[df["userid"] == user_id]
     return df_ret.to_dict(orient="records")
